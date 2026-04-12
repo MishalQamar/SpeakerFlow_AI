8d82595ce7df5f4a8d3afb9f03770369ed919dc2
Mishal Qamar <mishalqamar@gmail.com>
2026-04-12 20:11:56 +0100

    Initialize project foundation and service skeleton

---
+++ src/speakerflow_ai/tests/integration/test_web_app/test_main.py
@@ -0,0 +1,20 @@
+import pytest
+from fastapi.testclient import TestClient
+
+from speakerflow_ai.web_app.main import app
+
+
+@pytest.fixture
+def client():
+    return TestClient(app)
+
+
+def test_health_check(client):
+    """
+    GIVEN the web app is running
+    WHEN the health check endpoint is called with GET
+    THEN a 200 response with message OK is returned
+    """
+    response = client.get("/health-check/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "OK"}
